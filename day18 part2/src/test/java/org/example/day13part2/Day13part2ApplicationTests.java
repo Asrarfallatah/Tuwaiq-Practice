dfbc1fc085dc3689fa2e9c6a7e94a3bcbbf1dd03
ASRAR FALLATAH <sweetasuha21@gmail.com>
2025-12-20 12:27:52 +0300

    Add files via upload

---
+++ day18 part2/src/test/java/org/example/day13part2/Day13part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.day13part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13part2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
